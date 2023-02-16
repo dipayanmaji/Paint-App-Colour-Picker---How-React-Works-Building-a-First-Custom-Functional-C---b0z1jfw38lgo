9a92cf595924702e4cdc6bf84e0e98b85a8913c5
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-02-16 17:36:22 +0530

    solve assigment (1st trying)

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import "../styles/Child.css";
 
+const Selection = ({applyColor})=>{
+    return(
+        <div onClick={applyColor} className={"fix-box"}>
+            <h2 className={'subheading'}>Selection</h2>
+        </div>
+    );
+}
+export default Selection;
